5c7fe2adbefe251737e960f050fb5710fd24cab5
Kurnev Egor <kurnev.e@gmail.com>
2020-07-30 22:07:44 +0300

    WIP: Physics

--- src/hero.rs
+++ src/hero.rs
@@ -6,6 +6,11 @@ pub struct Hero {
     // hero is 3 tiles in height and 1 tile in width
     pub position: geometry::Rectangle,
     pub applied_forces: Vec<physics::DirectionalForce>,
+    pub cursorAngle: f64,
+}
+
+impl physics::Physics for Hero {
+    fn get_next_tick_position(&self) -> geometry::Rectangle {}
 }
 
 impl Hero {
@@ -15,7 +20,9 @@ impl Hero {
             applied_forces: vec![physics::DirectionalForce {
                 force: 1.0,
                 angle: PI / 2.0,
+                attenuation: 1.0, // g force baby
             }],
+            cursorAngle: 180.0,
         }
     }
 }
