bd5852b6555c908c841b194d73e7c4622f8ef519
Kurnev Egor <kurnev.e@gmail.com>
2020-07-30 22:07:25 +0300

    WIP: Physics

--- src/physics.rs
+++ src/physics.rs
@@ -1,5 +1,12 @@
+use crate::geometry;
+
 // Force is applied to center of the object
 pub struct DirectionalForce {
     pub force: f64,
-    pub angle: f64, // 0 at top, full circle is pi
+    pub angle: f64,       // 0 at right, full circle is pi
+    pub attenuation: f64, // how fast force weakens     % / 100
+}
+
+pub trait Physics {
+    fn get_next_tick_position(&self) -> geometry::Rectangle;
 }
